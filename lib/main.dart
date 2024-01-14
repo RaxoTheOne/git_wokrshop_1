a7a430d98ae4dbd6662575809f6e86da28981f88
RaxoTheOne <traxon.bg@gmail.com>
2024-01-14 20:04:52 +0100

    CustomContainer added with Text in it

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,38 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: CustomContainer(),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0), // Anpassen der Innenabstände des Containers
+      decoration: BoxDecoration(
+        color: Colors.blue, // Hintergrundfarbe des Containers
+        borderRadius: BorderRadius.circular(10.0), // Abrundung der Ecken des Containers
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5), // Schattenfarbe und Opazität
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3), // Schattenrichtung
+          ),
+        ],
+      ),
+      child: const Text(
+        'Hello World!',
+        style: TextStyle(
+          fontSize: 24.0, // Schriftgröße des Textes
+          color: Colors.white, // Textfarbe
+          fontWeight: FontWeight.bold, // Textgewicht
         ),
       ),
     );
